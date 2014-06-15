e3bed32c79bb29c16cef5ded1527e36645606036
William Bagayoko <wbagayoko@gmail.com>
2014-06-14 20:13:31 -0500

    build

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,52 @@
 var ngqc;
 
-ngqc = angular.module('ngq.controllers', []);
+ngqc = angular.module('ngq.controllers', ['ngq.services']);
 
-ngqc.controller('HomeCtrl', function() {});
+ngqc.controller('HomeCtrl', function(AnswerSrvc, $location) {
+  this.startQuiz = function() {
+    AnswerSrvc.answers = [];
+    return $location.path('/quiz');
+  };
+});
+
+ngqc.controller('QuizCtrl', function(QuestionSrvc, AnswerSrvc, index, $location) {
+  var question, submit;
+  question = this.question = QuestionSrvc.questions[index];
+  this.index = index + 1;
+  submit = function(selected) {
+    var newIndex;
+    newIndex = index + 1;
+    AnswerSrvc.answers[index] = {
+      id: question.id,
+      answer: selected
+    };
+    if (newIndex < QuestionSrvc.questions.length) {
+      return $location.path('/quiz/' + newIndex);
+    } else {
+      return $location.path('/results');
+    }
+  };
+  this.selectTrue = function() {
+    return submit(true);
+  };
+  this.selectFalse = function() {
+    return submit(false);
+  };
+});
+
+ngqc.controller('ResultsCtrl', function(QuestionSrvc, AnswerSrvc) {
+  this.questions = QuestionSrvc.questions;
+  this.answers = AnswerSrvc.answers;
+  this.isQuestionCorrect = function(index) {
+    return (AnswerSrvc.answers[index] != null) && AnswerSrvc.answers[index].answer === QuestionSrvc.questions[index].correct;
+  };
+  this.answerText = function(index) {
+    if (AnswerSrvc.answers[index] == null) {
+      return "Not answered";
+    } else if (AnswerSrvc.answers[index].answer) {
+      return "True";
+    } else {
+      return "False";
+    }
+  };
+});
--- app/js/routes.js
+++ app/js/routes.js
@@ -1,7 +1,33 @@
 ngq.config(function($routeProvider) {
   $routeProvider.when('/home', {
     templateUrl: 'partials/home.html',
-    controller: 'HomeCtrl'
+    controller: 'HomeCtrl',
+    controllerAs: 'h'
+  });
+  $routeProvider.when('/quiz', {
+    templateUrl: 'partials/quiz.html',
+    controller: 'QuizCtrl',
+    controllerAs: 'q',
+    resolve: {
+      index: function() {
+        return 0;
+      }
+    }
+  });
+  $routeProvider.when('/quiz/:index', {
+    templateUrl: 'partials/quiz.html',
+    controller: 'QuizCtrl',
+    controllerAs: 'q',
+    resolve: {
+      index: function($route) {
+        return parseInt($route.current.params.index, 10);
+      }
+    }
+  });
+  $routeProvider.when('/results', {
+    templateUrl: 'partials/results.html',
+    controller: 'ResultsCtrl',
+    controllerAs: 'r'
   });
   $routeProvider.otherwise({
     redirectTo: '/home'
